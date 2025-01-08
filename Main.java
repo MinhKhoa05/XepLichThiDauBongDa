87cd1d58ee50288f8e0dfce62ccd86801a14285f
MinhKhoa05 <mkhoa639@gmail.com>
2025-01-08 08:44:44 +0700

    Delete Main.java

--- Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-        System.out.println("Xin chào tui là Khoa, bạn cần giúp gì");
-    }
-}
